af69b8ffe7bbf824ef73c4120426d2766f1ea2d0
Jordi Prats <jprats@systemadmin.es>
2016-02-19 16:40:08 +0100

    afegida validació

--- manifests/extfs.pp
+++ manifests/extfs.pp
@@ -11,6 +11,8 @@ define tune2fs::extfs (
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
   }
 
+  validate_integer($max_mount_count)
+
   exec { "max_mount_count ${dev}":
     command => "tune2fs -c ${max_mount_count} ${dev}",
     unless  => "tune2fs -l ${dev} | grep 'Maximum mount count' | awk '{ print \$NF }' | grep '^${max_mount_count}$'",
@@ -39,13 +41,15 @@ define tune2fs::extfs (
   }
   else
   {
+    validate_integer($reserved_blocks)
+
     exec { "reserved_blocks ${dev}":
       command => "tune2fs -r ${reserved_blocks} ${dev}",
       unless  => "tune2fs -l ${dev} | grep 'Reserved block count' | awk '{ print \$NF }' | grep -E '^${reserved_blocks}$'",
     }
   }
 
-
+  validate_integer($check_interval)
 
   exec { "check_interval ${dev}":
     command => "tune2fs -i ${check_interval}s ${dev}",
